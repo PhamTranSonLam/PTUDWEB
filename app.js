860ece49d8ee1e8d3a3f470fc717a1404a54857f
John Doe <lamphamabc123@gmail.com>
2024-03-03 10:23:15 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const contactsRouter = require("./app/route/contact.route");
+const contactsRouter = require("./app/route/contact.route");
 // const ApiError = require("./app/api-error");
 
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 // app.use((req, res, next) => {
 //     return next(new ApiError(404, "Resource not found"));
